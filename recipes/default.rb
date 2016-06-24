926871a4691abe2804d1d2f181f73ffb4cefe750
Scott <sr@chef.io>
2016-06-24 17:44:50 +0300

    updated to work with srv-delivery user

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,16 @@ package 'httpd' do
   action :install
 end
 
+template '/etc/httpd/conf/httpd.conf' do
+  source 'httpd.conf.erb'
+  notifies :restart, 'service[httpd]'
+end
+
+template '/var/www/index.html' do
+  source 'index.html.erb'
+  mode '755'
+end
+
 service 'httpd' do
   action [ :enable, :start ]
 end
